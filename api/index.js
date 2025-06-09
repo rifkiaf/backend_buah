043bc22ac5f7a9340279ec8a96f5c7af2370aef7
rifkiaf <ahmadfahreizirifki@gmail.com>
2025-06-09 16:34:33 +0700

    ganti corsoptions

--- api/index.js
+++ api/index.js
@@ -1,19 +1,26 @@
+const serverless = require("serverless-http");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require("dotenv").config();
 
 const midtransRoutes = require("../routes/midtrans");
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Backend Buah API is running');
+app.get("/", (req, res) => {
+  res.send("Backend Buah API is running");
 });
 
 app.use("/api", midtransRoutes);
 
-// Export handler untuk Vercel
 module.exports = app;
+module.exports.handler = serverless(app);
